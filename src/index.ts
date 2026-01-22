b01eed7d81458b8f683a092990e0011534a5ec3d
muzammilmunir <muzammil_khania@hotmail.com>
2026-01-22 21:58:45 +0500

    type guard and enum

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+// import cors from 'cors';
 const app = express();
 import diaryRouter from './routes/diaries';
 app.use(express.json());
-
+// app.use(cors());
 const PORT = 3000;
 
 app.get('/ping', (_req, res) => {
